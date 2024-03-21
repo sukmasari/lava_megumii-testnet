19b06385285a0f3f0a316e1a2b521b46a8c215f3
sukmasari <137637174+sukmasari@users.noreply.github.com>
2024-03-21 21:17:17 +0700

    Update run.sh
    Update RPC

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-9cb450fa-0ee5-474f-87eb-34a27d4c9552/)
 
 
 echo "$curl_output" >> Update.tsx
